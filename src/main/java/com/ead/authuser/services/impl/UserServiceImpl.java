2c48295e567914e9db87fe582dc1a0ad57be968c
Renato Santana Brito <britotecst@outlook.com>
2022-08-12 21:34:52 -0300

    - implementado model - implementado repository - implementado service - implementado conexão com banco de dados postgres usando yaml - adicionado docker-compose para banco de dados postgres pgadmin

---
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.ead.authuser.services.impl;
+
+import com.ead.authuser.repositories.UserRepository;
+import com.ead.authuser.services.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
